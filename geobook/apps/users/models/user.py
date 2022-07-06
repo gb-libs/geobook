220434dd9147cc28bcf79a7355d2b614831b2a19
folt <foltfrend@list.ru>
2022-07-07 00:31:55 +0300

    :pen: up motor

--- geobook/apps/users/models/user.py
+++ geobook/apps/users/models/user.py
@@ -2,7 +2,7 @@ from geobook.db.backends.mongodb import models
 from pydantic import BaseConfig, Field, SecretStr
 
 
-class UserModel(models.MongoModel):
+class AbstractUserModel(models.MongoModel):
     username: str = Field(
         regex=r'^[\w.@+-]+\Z',
         min_length=1,
@@ -12,11 +12,11 @@ class UserModel(models.MongoModel):
         min_length=1)
 
 
-class UserWriteModel(UserModel):
+class UserWriteModel(AbstractUserModel):
     pass
 
 
-class UserReadModel(UserModel):
+class UserReadModel(AbstractUserModel):
     class Config(BaseConfig):
         fields = {
             'password': {'exclude': True},
